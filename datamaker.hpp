3a2866701b0fba1aabe07fcbccbb944e39efe254
Xuecheng Zhou <Zxc200611@yandex.com>
2022-01-19 15:12:22 +0800

    add randTree()

--- datamaker.hpp
+++ datamaker.hpp
@@ -65,3 +65,35 @@ vector<int> randSplit(int n,int sum,int minLimit=0,int maxLimit=2139062143)
 	shuffle(ans.begin(),ans.end(),Rand);
 	return ans;
 }
+inline 
+vector<vector<int>> randTree(int n)
+{
+	vector<vector<int>> ans;
+	vector<int> prf,deg;
+	set<int> lef;
+	ans.clear(),prf.clear(),deg.clear(),lef.clear();
+	for(int i=0;i<=n;i++)
+	{
+		deg.push_back(0);
+		ans.push_back(vector<int>({}));
+	}
+	for(int i=1;i<=n-2;i++)
+	{
+		prf.push_back(randNum(1,n));
+		deg[prf.back()]+=1;
+	}
+	for(int i=1;i<=n;i++)
+	{
+		if(deg[i]==0)
+			lef.insert(i);
+	}
+	for(int i=1;i<=n-2;i++)
+	{
+		ans[prf[i-1]].push_back(*lef.rbegin());
+		lef.erase(*lef.rbegin());
+		if((deg[prf[i-1]]-=1)==0)
+			lef.insert(prf[i-1]);
+	}
+	ans[*lef.begin()].push_back(n);
+	return ans;
+}
